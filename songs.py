124730a11aa653dcf2d6e851dfee59bdf53ed5da
Artem Shelest <artem.e.shelest@gmail.com>
2019-12-12 10:04:01 +0200

    Create composite CSV.

--- songs.py
+++ songs.py
@@ -6,9 +6,10 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-from fields import URL, ID, TITLE
+from fields import URL, ID, TITLE, TEXT
 
 OUT_PATH_TEMPLATE = "data/songs/{id}.txt"
+OUT_CSV_TEMPLATE = "{}_out.csv"
 
 
 def song_lyrics_from_url(url, remove_section_headers):
@@ -32,18 +33,29 @@ def song_lyrics_from_url(url, remove_section_headers):
 
 
 def run(input_csv: str) -> None:
-    with open(input_csv, mode='r') as f:
+    with open(input_csv, "r") as f:
+        out_csv_name = OUT_CSV_TEMPLATE.format(os.path.splitext(input_csv)[0])
         reader = csv.DictReader(f)
-        next(reader)
-        for row in reader:
-            fname = OUT_PATH_TEMPLATE.format(id=row[ID])
-            if row[URL] and not os.path.exists(fname):
-                print("Fetching {}".format(row[TITLE]))
-                text = song_lyrics_from_url(row[URL], True)
-                with open(fname, "w") as out_file:
-                    out_file.write(text)
-            else:
-                print("Skipping {}".format(row[TITLE]))
+        with open(out_csv_name, "w") as outf:
+            writer = csv.DictWriter(outf, reader.fieldnames + [TEXT])
+            writer.writeheader()
+            for row in reader:
+                fname = OUT_PATH_TEMPLATE.format(id=row[ID])
+                exists = os.path.exists(fname)
+                if row[URL] and not exists:
+                    print("Fetching {}".format(row[TITLE]))
+                    text = song_lyrics_from_url(row[URL], True)
+                    if text is not None:
+                        with open(fname, "w") as out_file:
+                            out_file.write(text)
+                            exists = True
+                else:
+                    print("Skipping {}".format(row[TITLE]))
+                if exists:
+                    with open(fname, "r") as in_file:
+                        text = "".join(in_file.readlines())
+                        row[TEXT] = text.replace("\n\r", " ").replace("\r\n", " ").replace("\r", " ").replace("\n", " ")
+                        writer.writerow(row)
 
 
 if __name__ == "__main__":
